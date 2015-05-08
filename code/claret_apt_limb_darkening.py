67049b325ca075a14097e3b4c5c7bdea94efae62
Matt Giguere <matthew.giguere@gmail.com>
2015-05-08 13:17:09 -0400

    fix to return correct ldfunc

--- code/claret_apt_limb_darkening.py
+++ code/claret_apt_limb_darkening.py
@@ -4,8 +4,7 @@
 PURPOSE:
 
 To return the theoretical Limb Darkening coefficients and/or model
-for observations taken with the MOST Satellite given the input
-parameters.
+for observations taken with APT given the input parameters.
 
 This code makes use of the Claret et al. (2013) A&A 552A, 16C
 paper, which should be cited if you use this code:
@@ -47,14 +46,6 @@ class LimbDarkening:
     :param logg:
         The log of the surface gravity
 
-    :param passband:
-        The passband of interest. Acceptable values are
-        ['Kp', 'C', 'S1', 'S2', 'S3', 'S4',
-         'u', 'v', 'b', 'y',
-         'U', 'B', 'V', 'R', 'I',
-         'J', 'H', 'K',
-         "u'", "g'", "r'", "i'", "z'",
-         'J2', 'H2', 'Ks']
     """
 
     def __init__(self, teff, logg, passband):
@@ -62,7 +53,6 @@ class LimbDarkening:
 
         self.teff = teff
         self.logg = logg
-        self.pb = passband
         self.round_values()
 
     def round_values(self):
@@ -80,7 +70,7 @@ class LimbDarkening:
         """
         return pd.read_csv(self.fn, sep='\s*\s*', engine='python', header=47)[2:]
 
-    def get_ldcs(self):
+    def get_ldcs(self, passband):
         """
         PURPOSE:
         This function will extract and return the limb darkening coefficients
@@ -93,57 +83,48 @@ class LimbDarkening:
         for cNm in coeffNames:
             ldcs[idx] = lddf[((lddf['Teff'] == '{:n}'.format(self.rteff)) &
                              (lddf['logg'] == '{:.2f}'.format(self.rlogg)) &
-                             (lddf['Filt'] == self.pb))][cNm].values[0]
+                             (lddf['Filt'] == passband))][cNm].values[0]
             idx += 1
         return ldcs
 
-    def claret_model(self, ldcs):
+    def claret_model(self):
+
+        ldcsb = self.get_ldcs('b')
+        ldcsy = self.get_ldcs('y')
+
         def limb_darkening_function(theta):
             mu = np.cos(theta)
-            limbdark = 1. - ldcs[0] * (1. - mu**0.5)
-            -  ldcs[1] * (1. - mu**1.0)
-            -  ldcs[2] * (1. - mu**1.5)
-            -  ldcs[3] * (1. - mu**2.0)
+            limbdark = 1. \
+                - (ldcsb[0] * (1. - mu**0.5) + ldcsy[0] * (1. - mu**0.5))/2. \
+                - (ldcsb[1] * (1. - mu**1.0) + ldcsy[1] * (1. - mu**1.0))/2. \
+                - (ldcsb[2] * (1. - mu**1.5) + ldcsy[2] * (1. - mu**1.5))/2. \
+                - (ldcsb[3] * (1. - mu**2.0) + ldcsy[3] * (1. - mu**2.0))/2.
             return limbdark
         return limb_darkening_function
 
 
-def claret13_limb_darkening_coeffs(teff, logg, passband):
+def claret_apt_limb_darkening_coeffs(teff, logg):
     """
     PURPOSE:
-    To round the input values in order to extract the limb darkening
+    To round the input values, extract the limb darkening
     coefficients from the Claret et al. (2013) table.
+
 
     :param teff:
     The stellar effective temperature in Kelvin
 
-    :param monh:
-    The stellar metalicity
-
     :param logg:
     The log of the stellar surface gravity
 
-    :param turbvel:
-    The stellar turbulent velocity
-
     See the Claret et al. (2013) paper for more details.
     """
-
-    # create an array of acceptable passband values:
-    pbs = ['Kp', 'C', 'S1', 'S2', 'S3', 'S4',
-           'u', 'v', 'b', 'y',
-           'U', 'B', 'V', 'R', 'I',
-           'J', 'H', 'K',
-           "u'", "g'", "r'", "i'", "z'",
-           'J2', 'H2', 'Ks']
 
     assert teff >= 5000, 'teff should be greater than 3000: {}'.format(teff)
     assert teff <= 10000, 'teff should be less than 50000: {}'.format(teff)
     assert logg >= 3.0, 'logg should be greater than 0.0: {}'.format(logg)
     assert logg <= 5.5, 'logg should be less than 5.0: {}'.format(logg)
-    assert passband in pbs, 'that is not an acceptable passband value'
 
-    ldo = LimbDarkening(teff, logg, passband)
+    ldo = LimbDarkening(teff, logg)
     print(ldo.get_ldcs())
 
 
@@ -156,13 +137,9 @@ if __name__ == '__main__':
     parser.add_argument(
         'logg',
         help='The log of the stellar surface gravity')
-    parser.add_argument(
-        'passband',
-        help='The filter/passband')
 
     args = parser.parse_args()
 
-    claret13_limb_darkening_coeffs(int(args.teff),
-                                   float(args.logg),
-                                   args.passband)
+    claret_apt_limb_darkening_coeffs(int(args.teff),
+                                     float(args.logg))
  
