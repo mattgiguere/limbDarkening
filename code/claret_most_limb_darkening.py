da8b433e3f26070ee49cd9a0228a88ca351cd63e
Matt Giguere <matthew.giguere@gmail.com>
2015-05-08 11:39:22 -0400

    now works at command line and as object

--- code/claret_most_limb_darkening.py
+++ code/claret_most_limb_darkening.py
@@ -48,82 +48,115 @@ class LimbDarkening:
         The log of the surface gravity
     """
 
+    def __init__(self, teff, logg, monh, turbvel):
+        self.fn = ld_dir+'data/Claret2014MOST_ATLAS.tsv'
 
-################################################################
-# USE THE CLARET LIMB DARKENING MODEL
-################################################################
-def getMOSTLDC(ld14fnm, monh, logg, teff, turbvel, passband):
-    """This function will extract the limb darkening coefficients
-    from the Claret table."""
-    ldcs = np.zeros(4)
-    coeffNames = ['a1', 'a2', 'a3', 'a4']
-    idx = 0
-    lddf = pd.read_csv(ld14fnm, sep='\s*\s*', engine='python', header=70)[2:]
-    for cNm in coeffNames:
-        ldcs[idx] = lddf[((lddf['Teff'] == '{:n}'.format(teff)) &
-                         (lddf['logg'] == '{:.2f}'.format(logg)) &
-                         (lddf['logZ'] == '{:.1f}'.format(monh)))][cNm].values[0]
-        idx += 1
-    return ldcs
+        self.teff = teff
+        self.monh = monh
+        self.logg = logg
+        self.turbvel = turbvel
+        self.round_values()
+
+    def round_values(self):
+        """
+        PURPOSE:
+        To round the input parameters for lookup in the Claret table.
+        """
+        self.rteff = np.round(self.teff/250) * 250
+        self.rmonh = np.round(self.monh/0.1) * 0.1
+        self.rlogg = np.round(self.logg/0.5) * 0.5
+
+    def get_data(self):
+        """
+        PURPOSE:
+        Restore the data from file and return them as a pandas DataFrame.
+        """
+        return pd.read_csv(self.fn, sep='\s*\s*', engine='python', header=70)[2:]
+
+    def get_ldcs(self):
+        """
+        PURPOSE:
+        This function will extract and return the limb darkening coefficients
+        from the Claret table.
+        """
+        ldcs = np.zeros(4)
+        coeffNames = ['a1', 'a2', 'a3', 'a4']
+        idx = 0
+        lddf = self.get_data()
+        for cNm in coeffNames:
+            ldcs[idx] = lddf[((lddf['Teff'] == '{:n}'.format(self.rteff)) &
+                             (lddf['logg'] == '{:.2f}'.format(self.rlogg)) &
+                             (lddf['logZ'] == '{:.1f}'.format(self.rmonh)))][cNm].values[0]
+            idx += 1
+        return ldcs
+
+    def claret_model(self, ldcs):
+        def limb_darkening_function(theta):
+            mu = np.cos(theta)
+            limbdark = 1. - ldcs[0] * (1. - mu**0.5)
+            -  ldcs[1] * (1. - mu**1.0)
+            -  ldcs[2] * (1. - mu**1.5)
+            -  ldcs[3] * (1. - mu**2.0)
+            return limbdark
+        return limb_darkening_function
 
 
-def claret14_limb_darkening_coeffs():
-    ld14fnm = ld_dir+'data/Claret2014MOST_ATLAS.tsv'
+def claret14_limb_darkening_coeffs(teff, monh, logg, turbvel):
+    """
+    PURPOSE:
+    To round the input values in order to extract the limb darkening
+    coefficients from the Claret et al. (2014) table.
 
-    teff = 5084.
+    :param teff:
+    The stellar effective temperature in Kelvin
 
-    # specify the metalicity for the Claret Models (log [M/H]):
-    monh = -0.13
+    :param monh:
+    The stellar metalicity
 
-    # specify the surface gravity:
-    logg = 4.3
+    :param logg:
+    The log of the stellar surface gravity
 
-    # Turbulent Velocity for Claret Limn Dark Models (km/s):
-    turbvel = 2.0
+    :param turbvel:
+    The stellar turbulent velocity
 
-    # Round the setllar parameters for tabular lookup from
-    # the Claret models for Limb Darkening:
-    rteff = np.round(teff/250) * 250
-    rmonh = np.round(monh/0.1) * 0.1
-    rlogg = np.round(logg/0.5) * 0.5
+    See the Claret et al. (2014) paper for more details.
+    """
 
-    ldcs = getMOSTLDC(ld14fnm, rmonh, rlogg, rteff, turbvel, passband)
-    return ldcs
+    assert teff >= 3000, 'teff should be greater than 3000: {}'.format(teff)
+    assert teff <= 50000, 'teff should be less than 50000: {}'.format(teff)
+    assert monh >= -5.0, 'monh should be greater than -5.0: {}'.format(monh)
+    assert monh <= 1.0, 'monh should be greater than 1.0: {}'.format(monh)
+    assert logg >= 0.0, 'logg should be greater than 0.0: {}'.format(logg)
+    assert logg <= 5.0, 'logg should be less than 5.0: {}'.format(logg)
+    assert turbvel <= 8.0, 'turbvel should be less than 8.0: {}'.format(turbvel)
+    assert turbvel >= 0.0, 'turbvel should be greater than 0.0: {}'.format(turbvel)
 
-
-def claret_model(ldcs):
-
-    def limb_darkening_function(theta):
-        mu = np.cos(theta)
-        limbdark = 1. - ldcs[0] * (1. - mu**0.5)
-        -  ldcs[1] * (1. - mu**1.0)
-        -  ldcs[2] * (1. - mu**1.5)
-        -  ldcs[3] * (1. - mu**2.0)
-        return limbdark
-    return limb_darkening_function
-
-ldcs = claret14_limb_darkening_coeffs()
-ld14func = claret_model(ldcs)
-
+    print('input teff: {}'.format(teff))
+    print('input monh: {}'.format(monh))
+    print('input logg: {}'.format(logg))
+    print('input turbvel: {}'.format(turbvel))
+    ldo = LimbDarkening(teff, monh, logg, turbvel)
+    print(ldo.get_ldcs())
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='argparse object.')
     parser.add_argument(
-        'arg1',
-        help='This argument does something.')
+        'teff',
+        help='The stellar effective temperature.')
     parser.add_argument(
-        'arg2',
-        help='This argument does something else. By specifying ' +
-             'the "nargs=>" makes this argument not required.',
-             nargs='?')
-    if len(sys.argv) > 3:
-        print('use the command')
-        print('python filename.py tablenum columnnum')
-        sys.exit(2)
+        'monh',
+        help='The stellar metalicity')
+    parser.add_argument(
+        'logg',
+        help='The log of the stellar surface gravity')
+    parser.add_argument(
+        'turbvel',
+        help='The stellar turbulent velocity')
 
     args = parser.parse_args()
 
-    claret_most_limb_darkening(int(args.arg1), args.arg2)
+    claret14_limb_darkening_coeffs(int(args.teff), float(args.monh),
+                                   float(args.logg), float(args.turbvel))
  
